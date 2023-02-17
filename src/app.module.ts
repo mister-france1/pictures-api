4c29573f16a4116882126720987811bf148e02dc
ruslan <Ruslan.Onoprienko@hedy-agency.com>
2023-02-17 15:19:00 +0200

    add auth module and etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import * as Joi from '@hapi/joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { FilesModule } from './files/files.module';
-import * as Joi from '@hapi/joi';
+import { FilesModule } from './modules/files/files.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -12,10 +13,13 @@ import * as Joi from '@hapi/joi';
         AWS_REGION: Joi.string().required(),
         AWS_ACCESS_KEY_ID: Joi.string().required(),
         AWS_SECRET_ACCESS_KEY: Joi.string().required(),
-        AWS_PUBLIC_BUCKET_NAME: Joi.string().required()
+        AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
+        AWS_COGNITO_USER_POOL_ID: Joi.string().required(),
+        AWS_COGNITO_CLIENT_ID: Joi.string().required(),
       })
     }),
-    FilesModule
+    FilesModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'aws-sdk';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
   config.update({
